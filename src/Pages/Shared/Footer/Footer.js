e18e091aeb0dce794a9d68f0a404c2d3ffc1610f
JubayerDev <ajashes858@gmail.com>
2022-12-12 19:35:44 +0600

    header added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,57 +1,54 @@
 import React from "react";
 import logo from "../../../assets/logo.svg";
+import { AiFillGoogleCircle, AiFillTwitterCircle, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
-      <div>
-        <img src={logo} alt="logo" />
-        <p>
+    <footer className="footer py-32 px-20 bg-[#151515] text-base-content">
+      <div className="w-72">
+        <img src={logo} alt="logo" className="w-20"/>
+        <p className="my-5 text-[#E8E8E8]">
           Edwin Diaz is a software and web technologies engineer, a life coach
           trainer who is also a serial .
               </p>
-              
+              <div className="flex items-center text-[#E8E8E8]">
+                  <AiFillGoogleCircle size={30} className="cursor-pointer"/>
+                  <AiFillTwitterCircle size={30} className="cursor-pointer"/>
+                  <AiFillInstagram size={30} className="cursor-pointer"/>
+                  <AiFillLinkedin size={30} className="cursor-pointer"/>  
+              </div>
       </div>
-      <div>
-        <span className="footer-title">Services</span>
+      <div className="text-[#E8E8E8]">
+        <span className="font-bold text-xl text-white">About</span>
         <a href="/" className="link link-hover">
-          Branding
+          Home
         </a>
         <a href="/" className="link link-hover">
-          Design
-        </a>
-        <a href="/" className="link link-hover">
-          Marketing
-        </a>
-        <a href="/" className="link link-hover">
-          Advertisement
-        </a>
-      </div>
-      <div>
-        <span className="footer-title">Company</span>
-        <a href="/" className="link link-hover">
-          About us
+          Service
         </a>
         <a href="/" className="link link-hover">
           Contact
         </a>
+      </div>
+      <div className="text-[#E8E8E8]">
+        <span className="font-bold text-xl text-white">Company</span>
         <a href="/" className="link link-hover">
-          Jobs
+          Why Car Doctor
         </a>
         <a href="/" className="link link-hover">
-          Press kit
+          About
         </a>
       </div>
-      <div>
-        <span className="footer-title">Legal</span>
+      <div className="text-[#E8E8E8]">
+        <span className="font-bold text-xl text-white">Support</span>
         <a href="/" className="link link-hover">
-          Terms of use
+          Support Center
         </a>
         <a href="/" className="link link-hover">
-          Privacy policy
+          Feedback
         </a>
         <a href="/" className="link link-hover">
-          Cookie policy
+        Accessbility
         </a>
       </div>
     </footer>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../../../assets/logo.svg';
+import { BsHandbag } from 'react-icons/bs';
+import { IoIosSearch } from 'react-icons/io';
 
 const Header = () => {
-    return (
-        <div>
-            Header
+    const menuItems = <>
+        <li className='hover:text-[#FF3811]'><Link to='/'>Home</Link></li>
+        <li className='hover:text-[#FF3811]'><Link to='/about'>About</Link></li>
+        <li className='hover:text-[#FF3811]'><Link to='/services'>Services</Link></li>
+        <li className='hover:text-[#FF3811]'><Link to='/blog'>Blog</Link></li>
+        <li className='hover:text-[#FF3811]'><Link to='/contact'>Contact</Link></li>
+    </>
+  return (
+    <div className="navbar bg-base-100 h-86 mb-12">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuItems}
+          </ul>
         </div>
-    );
+        <Link to='/' className=""><img className="w-20" src={logo} alt="" /></Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="md:space-x-6 menu-horizontal px-1 font-semibold">
+          {menuItems}
+        </ul>
+      </div>
+          <div className="navbar-end space-x-4">
+              <BsHandbag size={24}/>
+              <IoIosSearch size={24}/>
+        <button className="btn btn-outline rounded-sm text-[#FF3811] hover:bg-[#FF3811] hover:text-white hover:border-white no-animation">Appointment</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
